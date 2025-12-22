fb8ccf556436fe739ce2f2a41262a353e7a96315
Nikhil Dhole <nikhildadadhole@gmail.com>
2025-12-22 22:31:47 +0530

    remove unwanted orchestrator

--- tests/graph/agents/orchestrator_test.py
+++
@@ -1,8 +0,0 @@
-def test_orchestrator():
-    from fastapi_lang_graph.graph.agents.orchestrator import orchestrator_agent, Route
-
-    response = orchestrator_agent.invoke({"messages": [{"role": "user", "content": "Draw a mermaid diagram for a simple web app."}]})
-    assert isinstance(response, dict)
-    assert isinstance(response.get("structured_response"), Route)
-    assert isinstance(response.get("structured_response").next_agent, str)
-    assert response.get("structured_response").next_agent == "mermaid_generator"
